511b280a8f15a077478e1e5da1b1e916f46d69bd
Siddhant-Baranwal <siddhantbr10@gmail.com>
2024-07-10 16:30:40 +0530

    adding logo to navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <a href="/">Home</a>
+      <a href="/" className='logosup'><img className='logo' src="/images/logo.png" alt="" />Home</a>
       <a href="/about">About</a>
       <a href="/contact">Contact</a>
     </div>
