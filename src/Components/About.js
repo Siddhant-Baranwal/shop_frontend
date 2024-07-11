e0207747754b4aafb9b55e0af1445c5dc48bac21
Siddhant-Baranwal <siddhantbr10@gmail.com>
2024-07-11 19:42:54 +0530

    editing about page content

--- src/Components/About.js
+++ src/Components/About.js
@@ -10,7 +10,7 @@ const About = () => {
       <h2>&#8226; Our mission</h2>
       <p>Our mission is to provide IIT BHU freshers with a one-step solution for all their basic needs, making their transition to college life smooth and hassle-free.</p>
       <h2>&#8226; Why trust us!</h2>
-      <p>We, the developers of DormBuddy, are from the same college as yours. So, in case of any problem you face while buying, you can <Link to="/contact" className='spe'>contact</Link> us online as well as offline.</p>
+      <p>We, the developers of DormBuddy, are from the same college as yours. So, in case of any problem you face while buying, you can <Link to="/contact" className='spe'>contact</Link> us online as well as offline. Also, if you have any issue related to the quality of product, we can show you the products offline when you come at the campus. All you have to do is just to message us and ask to see a sample piece of the product.</p>
       <h2>&#8226; What we offer</h2>
       <p>From essential dormitory supplies like mattress and buckets to tech gadgets and bicycles, we cover all the necessities to help you settle in comfortably.</p>
       <h2>&#8226; How it works</h2>
